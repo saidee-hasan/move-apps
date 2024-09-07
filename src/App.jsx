869497507bc33d1a828bcd6242a2ee5494015649
saideeWD <mdsaideehasan@gmail.com>
2024-09-07 13:05:10 +0600

    spainer

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,44 @@ import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import { router } from "./Routes/Router";
 import Layout from "./Layout/Layout";
+import { useEffect, useState } from "react";
+import { div, h1 } from "framer-motion/client";
+import Spiner from "./components/Spiner/Spiner";
 
 function App() {
+  const [text,setText] = useState('');
+  const [showImg,setShowImg]= useState(true);
+useEffect(()=>{
+
+  setTimeout(()=>{
+      setShowImg(false)
+    setText('ffff')
+  },800)
+},[])
+
+
+
   return (
     <>
-      <RouterProvider router={router}>
+       {
+          showImg ? (
+            
+           <img className='mx-auto' src={'https://cdn.pixabay.com/animation/2023/05/02/04/29/04-29-06-428_512.gif'}/>
+
+
+          ):
+          <div>
+             <RouterProvider router={router}>
+        
         <Layout></Layout>
       </RouterProvider>
+          </div>
+  
+        }
+
+
+ 
+     
     </>
   );
 }
---
+++ src/components/Spiner/Spiner.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+import Logo from"../../assets/icon/hero.png"
+const Spiner = () => {
+ 
+    return (
+        <div> 
+     
+ 
+            
+        </div>
+    );
+};
+
+export default Spiner;
